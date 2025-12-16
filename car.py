fae2833d3d918a9bf4252a0df4b5a50de360ae7c
yalda hashimi <yaldahashemi02@gmail.com>
2025-12-16 09:27:40 +0100

    new car

--- car.py
+++ car.py
@@ -1,33 +1,54 @@
+import time
+from picarx import Picarx
 from motor import MotorController
-from sensors import SensorManager
+from sensor import SensorManager
 from debug import Debugger
-from errors import ErrorHandler
+from error import ErrorHandler
+
+SAFE_DISTANCE = 40
+DANGER_DISTANCE = 25
+POWER = 50
 
 class Car:
     def __init__(self, debug=False):
         self.debugger = Debugger(debug)
         self.motor = MotorController()
-        self.sensors = SensorManager()
-        self.errors = ErrorHandler()
+        self.sensor = SensorManager()
+        self.error = ErrorHandler()
+        self.steering_angle = 0
+        # PicarX-Objekt für direkte Steuerung
+        self.px = Picarx()
 
     def safe_drive_step(self):
-        distance = self.sensors.get_distance()
-
-        if distance is None:
-            self.errors.register("Sensor liefert keine Werte.")
-            self.motor.stop()
+        try:
+            distance = round(self.px.ultrasonic.read(), 2)
+            self.debugger.log(f"Distance: {distance} cm")
+        except:
+            self.px.stop()
             return
 
-        if distance < 10:
-            self.debugger.log("Hindernis sehr nah – Notstopp!")
-            self.motor.stop()
+        if distance >= SAFE_DISTANCE:
+            self.px.set_dir_servo_angle(0)
+            self.px.forward(POWER)
+
+        elif distance >= DANGER_DISTANCE:
+            self.px.stop()
+            time.sleep(0.05)
+            self.px.set_dir_servo_angle(30)
+            self.px.forward(20)
+
         else:
-            self.motor.set_speed(40)
-            self.debugger.log(f"Speed gesetzt: {self.motor.speed}")
+            self.px.stop()
+            time.sleep(0)
+            self.px.set_dir_servo_angle(-30)
+            self.px.backward(POWER)
+            time.sleep(1)
+            self.px.stop()
 
     def get_state(self):
         return {
             "speed": self.motor.speed,
-            "distance": self.sensors.get_distance(),
-            "error": self.errors.get_last_error()
+            "distance": self.sensor.get_distance(),
+            "steering_angle": self.steering_angle,
+            "error": self.error.get_last_error()
         }
