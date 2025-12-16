ee9813f89e6258015eb8f32ef71842f64c8db2ca
yalda hashimi <yaldahashemi02@gmail.com>
2025-12-16 09:25:32 +0100

    new main

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import time
 
 car = Car(debug=True)
 
-while True:
-    car.safe_drive_step()
-    print(car.get_state())
-    time.sleep(1)
+try:
+    while True:
+        car.safe_drive_step()
+        print(car.get_state())
+        time.sleep(1)
+except KeyboardInterrupt:
+        car.px.forward(0)
+        car.motor.stop()
+        print("Programm beendet")
